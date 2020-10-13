cee5c5e8bab951ef87f58bb25b277210219b428f
Davyd Holovii <mirage.present@gmail.com>
2020-10-13 22:00:24 +0300

    Added help command

--- src/commands.py
+++ src/commands.py
@@ -4,6 +4,10 @@ def start_command_handler(update, context):
     print("/start command handler runs...")
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+def help_command_handler(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="This is a test bot")
+
 def register_commands(dispatcher):
     print("Registering commands")
     dispatcher.add_handler(CommandHandler('start', start_command_handler))
+    dispatcher.add_handler(CommandHandler('help', help_command_handler))
